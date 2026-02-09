7209ec76e7a78c542c8ec9e4428cf5095a29aee1
wasdprateek <112349674+wasdprateek@users.noreply.github.com>
2026-02-09 18:17:58 +0530

    START

---
+++ CurrencyConverter/CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Prateek Kumar Rai on 09/02/26.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
